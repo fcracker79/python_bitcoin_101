9f8e2f477c20cfb24b887798bbd4e25072b146a4
fcracker79 <fcracker79@gmail.com>
2018-04-09 23:20:25 +0200

    Completed BIP32

--- python/bitcoin_101/deterministic.py
+++ python/bitcoin_101/deterministic.py
@@ -13,36 +13,46 @@ def _derived_key(master_key: str, *path: int) -> str:
     return k
 
 
-def _derive_and_print(master_key: str, *path: int, derive_pub_key: bool=True):
+def _derive_and_print(master_key: str, *path: int, derive_pub_key: bool=True, magicbyte: int=0):
     master_pub_key = _derived_key(bitcoin.bip32_privtopub(master_key))
     derived_key = _derived_key(master_key, *path)
     if derive_pub_key:
-        derived_pub_key = _derived_key(master_pub_key, *path)
+        derived_bip32_pub_key = _derived_key(master_pub_key, *path)
+        derived_pub_key = bitcoin.bip32_extract_key(derived_bip32_pub_key)
     else:
-        derived_pub_key = 'N.A.'
+        derived_bip32_pub_key = derived_pub_key = 'N.A.'
     derived_pub_key_from_key = bitcoin.bip32_privtopub(derived_key)
     print(
         '''    Derivation path: ({}),
+    Derived BIP32 key: {},
+    Derived BIP32 public key: {},
+    BIP32 public key from derived BIP32 private: {},
     Derived key: {},
     Derived public key: {},
-    Derived public key from private: {}
+    Public key from derived key: {},
+    BTC address: {}
     '''
         .format(
             ', '.join(str(x) if x <= 2 ** 31 else str(x - 2 ** 31) + '\'' for x in path),
-            master_key,
-            derived_key, derived_pub_key, derived_pub_key_from_key))
+            derived_key, derived_bip32_pub_key, derived_pub_key_from_key,
+            bitcoin.bip32_extract_key(derived_key),
+            derived_pub_key,
+            bitcoin.privtopub(bitcoin.bip32_extract_key(derived_key)),
+            bitcoin.pubtoaddr(bitcoin.privtopub(bitcoin.bip32_extract_key(derived_key)), magicbyte=magicbyte)
+        )
+    )
 
 
-def _print_master_key_and_derived(vbytes: bytes):
+def _print_master_key_and_derived(vbytes: bytes, magicbyte: int):
     seed = _mnemonic_to_seed(b'This is a very funny mnemonic')
     master_key = bitcoin.bip32_master_key(seed, vbytes=vbytes)
     print('Master key:', master_key)
-    _derive_and_print(master_key, 1, 2, 3)
-    _derive_and_print(master_key, 1 + 2 ** 31, 2, 3, derive_pub_key=False)
+    _derive_and_print(master_key, 1, 2, 3, magicbyte=magicbyte)
+    _derive_and_print(master_key, 1 + 2 ** 31, 2, 3, derive_pub_key=False, magicbyte=magicbyte)
 
 
 if __name__ == '__main__':
     print('\nMAINNET\n')
-    _print_master_key_and_derived(bitcoin.MAINNET_PRIVATE)
+    _print_master_key_and_derived(bitcoin.MAINNET_PRIVATE, 0)
     print('\nTESTNET\n')
-    _print_master_key_and_derived(bitcoin.TESTNET_PRIVATE)
+    _print_master_key_and_derived(bitcoin.TESTNET_PRIVATE, 111)
